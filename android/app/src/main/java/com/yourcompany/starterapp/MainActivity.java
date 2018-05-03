b940fe102da8e796c3aeacac8d50b77018cc6bda
NJ-Developer <navsingh191@gmail.com>
2018-05-03 12:30:49 -0400

    english words package used to get random words.

--- android/app/src/main/java/com/yourcompany/starterapp/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/starterapp/MainActivity.java
@@ -1,14 +1,14 @@
 package com.yourcompany.starterapp;
-
 import android.os.Bundle;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-
 public class MainActivity extends FlutterActivity {
-  @Override
+   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    int a = 10;
+       System.out.println("A is: "+a );
+
   }
 }
