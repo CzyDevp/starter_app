81ae130421728e56a6e4ee4ddb38205e02dc68ab
NJ-Developer <navsingh191@gmail.com>
2018-05-03 14:22:31 -0400

    saving favourite words done!! New page will show saved words!! I love hot-reloading

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,21 @@ class MyApp extends StatelessWidget {
     //final wordPair = new WordPair.random();  before statefull class
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Flutter Startup'),
-        ),
-        body: new Center(
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: new RandomWords(),
+        //new Scaffold(
+        //appBar: new AppBar(
+       //   title: new Text('Flutter Startup'),
+       // ),
+       // body:
+        //new Center(
          // child: new Text('Hello World'),
           //child: new Text(wordPair.asPascalCase)
-          child: new RandomWords()
-        ),
-      ),
+         // child: new RandomWords()
+       // ),
+     // ),
     );
   }
 }
@@ -29,10 +34,73 @@ class RandomWords extends StatefulWidget{
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];  //save favourite words from list
   final _bigFont = const TextStyle(fontSize: 18.0);  //font size
+  final _saved = new Set<WordPair>(); //storing favourite words here
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+   // final wordPair = new WordPair.random();
+    //return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Random Word Generator'),
+        actions: <Widget>[new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)], //list icon
+      ),
+        body : _buildSuggestions(),
+
+    );
   }
-}
+  void _pushSaved(){
+     Navigator.of(context).push(
+       new MaterialPageRoute(builder: (context){
+         final tiles = _saved.map((pair){
+           return new ListTile(
+             title: new Text(pair.asPascalCase,style: _bigFont,),
+           );
+         },);
+         final divided = ListTile.divideTiles(context: context,
+         tiles: tiles,).toList();
+         return new Scaffold(
+           appBar: new AppBar(
+             title: new Text('Saved Suggestions'),
+           ),
+           body: new ListView(children: divided),
+         );
+       })
+     );
+
+  }
+  Widget _buildSuggestions(){
+    return new ListView.builder(padding: const EdgeInsets.all(16.0),
+    itemBuilder: (context,i){
+     if(i.isOdd){
+       return new Divider();
+    }
+    final index = i ~/2;
+     if(index>= _suggestions.length){
+       _suggestions.addAll(generateWordPairs().take(10)); //add 10 new pairs to suggestions
+    }
+     return _buildRow(_suggestions[index]);
+    }
+    );
+  }
+  Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(pair.asPascalCase,style: _bigFont,),
+      trailing: new Icon(alreadySaved ? Icons.favorite: Icons.favorite_border,
+      color: alreadySaved ? Colors.red: Colors.green,),
+      onTap: (){
+        setState((){
+          if(alreadySaved){
+            _saved.remove(pair);
+          }
+          else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+
+  }
+
+   }
